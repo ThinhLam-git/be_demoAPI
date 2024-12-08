105accda6ee2587b338e104835977326b0a48e6c
Tuấn Thịnh <ltthinh111@gmail.com>
2024-12-08 17:30:13 +0700

    update

--- api.py
+++ api.py
@@ -113,4 +113,5 @@ def run_clustering():
     return jsonify(result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Cổng từ môi trường
+    app.run(host='0.0.0.0', port=port)  # Flask sẽ lắng nghe trên cổng này
